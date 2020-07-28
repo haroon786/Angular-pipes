9c3c15c9f578c11c0606ebbb3a745749c8497996
HaroonJ <haroon.ansari123@gmail.com>
2020-07-28 23:19:50 +0530

    styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrefixPipe } from './prefix.pipe';
+import { ChildComponent } from './child/child.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrefixPipe
+    PrefixPipe,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
